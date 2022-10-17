4c8a8933352b1a375ae41d8d7a8ef3d0ea32a4b4
coder-ms <marco.sbrascia@gmail.com>
2022-10-17 16:38:40 +0300

    aggiunta imge e commenti principali allo script.js

--- js/script.js
+++ js/script.js
@@ -10,3 +10,14 @@ Aggiungere una select accanto al bottone di generazione, che fornisca una scelta
 - con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
 - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
 */
+
+/*Function play: funzione principale dove viene strutturata la griglia di grancìdezza 7*7, 9*9, e 10*10 */
+
+    /*switch case level 1 2 or 3 */
+
+    /*function drawCell: funzione interna alla function play che genera le celle */
+    /*ciclo for per creazione bombe: richiamo dalla libreria.js la function randomNumber*/ 
+
+    /*function drawGrid: funzione interna alla function play che genera il terreno di gioco */
+    /*Richiamo la funzione drawgrid */
+
